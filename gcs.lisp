74abf0a16c20a707d547355a23781a5ec9956860
Pierre THIERRY <nowhere.man@levallois.eu.org>
2010-05-01 19:04:09 +0200

    Added ADD-ENTITY

--- gcs.lisp
+++ gcs.lisp
@@ -17,6 +17,12 @@
 (define-hash-table-reader geometrical-constraints-system get-entity entities)
 (define-hash-table-writer geometrical-constraints-system store-entity entities)
 
+(define-hash-table-reader geometrical-constraints-system get-unknown unknowns)
+(define-hash-table-writer geometrical-constraints-system store-unknown unknowns)
+
+(define-hash-table-reader geometrical-constraints-system get-parameter parameters)
+(define-hash-table-writer geometrical-constraints-system store-parameter parameters)
+
 (define-hash-table-reader geometrical-constraints-system get-predicate-criterion predicate-criteria)
 (define-hash-table-writer geometrical-constraints-system store-predicate-criterion predicate-criteria)
 
@@ -26,3 +32,10 @@
 
 (defmethod has-entity-p ((system geometrical-constraints-system) entity)
   (gethash entity (slot-value system 'entities))
+
+(defmethod add-entity ((system geometrical-constraints-system) entity type unknown)
+  (let ((obj (make-instance 'geometrical-entity :type type)))
+    (store-entity system entity obj)
+    (if unknown
+	(store-unknown system entity obj)
+	(store-parameter system entity obj))))
