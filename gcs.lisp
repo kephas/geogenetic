1cbad4ef3a20c63b6bdad9ce10a0ae2ee923d7a2
Pierre THIERRY <nowhere.man@levallois.eu.org>
2010-05-01 18:37:03 +0200

    Added part of GCS object definition

---
+++ gcs.lisp
@@ -0,0 +1,17 @@
+(defclass geometrical-constraints-system ()
+  ((entities :initform (make-hash-table))
+   (unknowns :initform (make-hash-table))
+   (parameters :initform (make-hash-table))
+   (predicate-criteria :initform (make-hash-table))
+   (predicate-signatures :initform (make-hash-table))))
+
+
+(defmacro define-hash-table-reader (class name slot)
+  `(defmethod ,name ((object ,class) key)
+     (gethash key (slot-value object ',slot))))
+
+(define-hash-table-reader geometrical-constraints-system get-entity entities)
+
+
+(defmethod has-entity-p ((system geometrical-constraints-system) entity)
+  (gethash entity (slot-value system 'entities))
