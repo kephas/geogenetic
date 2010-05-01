3ed5ea2579fe0f4bc0f37fe463ba72c7d998124c
Pierre THIERRY <nowhere.man@levallois.eu.org>
2010-05-01 18:57:57 +0200

    Added macro for hash table writer methods and used it

--- gcs.lisp
+++ gcs.lisp
@@ -10,9 +10,18 @@
   `(defmethod ,name ((object ,class) key)
      (gethash key (slot-value object ',slot))))
 
+(defmacro define-hash-table-writer (class name slot)
+  `(defmethod ,name ((object ,class) key value)
+     (setf (gethash key (slot-value object ',slot)) value)))
+
 (define-hash-table-reader geometrical-constraints-system get-entity entities)
+(define-hash-table-writer geometrical-constraints-system store-entity entities)
+
 (define-hash-table-reader geometrical-constraints-system get-predicate-criterion predicate-criteria)
+(define-hash-table-writer geometrical-constraints-system store-predicate-criterion predicate-criteria)
+
 (define-hash-table-reader geometrical-constraints-system get-predicate-signature predicate-signatures)
+(define-hash-table-writer geometrical-constraints-system store-predicate-signature predicate-signatures)
 
 
 (defmethod has-entity-p ((system geometrical-constraints-system) entity)
