1d285eb76622d544234631e0e0ead926bb4d7108
Pierre THIERRY <nowhere.man@levallois.eu.org>
2010-05-13 19:58:22 +0200

    Added WITH-FUNCTIONS macro

--- macros.lisp
+++ macros.lisp
@@ -1,3 +1,11 @@
+(defmacro with-functions (names &body body)
+  `(labels (,@(mapcar (lambda (name-spec)
+                        (if (consp name-spec)
+                            `(,(car name-spec) (&rest rest) (apply ,(cadr name-spec) rest))
+                            `(,name-spec (&rest rest) (apply ,name-spec rest))))
+                      names))
+     ,@body))
+
 (defmacro named-let (name binds &body body)
   `(labels ((,name ,(mapcar #'first binds) ,@body))
      (,name ,@(mapcar #'second binds))))
