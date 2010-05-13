8e1b047712158896489e0b212205b8ec2a581d74
Pierre THIERRY <nowhere.man@levallois.eu.org>
2010-05-13 20:14:24 +0200

    Used MAPCAN in EVOLVE

--- evolve.lisp
+++ evolve.lisp
@@ -1,2 +1,2 @@
 (defun evolve (population select crossover mutate)
-  (mapcar mutate (apply #'append (mapcar crossover (funcall select population)))))
+  (mapcar mutate (mapcan crossover (funcall select population))))
