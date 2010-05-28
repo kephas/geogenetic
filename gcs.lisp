1c7865a344c4fbc7967cf71561f1a749d353aa96
Pierre THIERRY <nowhere.man@levallois.eu.org>
2010-05-28 18:23:33 +0200

    Added some delegated methods from GCS to its universe

--- gcs.lisp
+++ gcs.lisp
@@ -1,5 +1,6 @@
 (defclass geometrical-constraints-system ()
-  ((entities :initform (make-hash-table))
+  ((universe :initarg :universe :reader gcs-universe)
+   (entities :initform (make-hash-table))
    (unknowns :initform (make-hash-table))
    (parameters :initform (make-hash-table))))
 
@@ -12,6 +13,12 @@
 (define-hash-table-reader geometrical-constraints-system get-parameter parameters)
 (define-hash-table-writer geometrical-constraints-system store-parameter parameters)
 
+
+(defmethod get-predicate-criterion ((gcs geometrical-constraints-system) predicate)
+  (get-predicate-criterion (gcs-universe gcs) predicate))
+
+(defmethod predicate-signature ((gcs geometrical-constraints-system) predicate)
+  (get-predicate-signature (gcs-universe gcs) predicate))
 
 (defmethod has-entity-p ((system geometrical-constraints-system) entity)
   (gethash entity (slot-value system 'entities)))
