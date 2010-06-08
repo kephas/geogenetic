0e42d7761391c82a1c561c13246e2079a7b3b088
Pierre Thierry <pierre@nothos.net>
2010-06-08 15:50:45 +0200

    Added DISTANCE-BETWEEN

--- misc.lisp
+++ misc.lisp
@@ -2,3 +2,9 @@
 
 (defun error-margin (val1 val2)
   (abs (- val1 val2)))
+
+(defun distance-between (point1 point2)
+  (sqrt (reduce #'+ (mapcar (lambda (coord1 coord2)
+			      (expt (abs (- coord1 coord2)) 2))
+			    (point-coordinates point1)
+			    (point-coordinates point2)))))
