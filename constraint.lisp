ff4a75e4cf1d61483aef3ac5929ca73546d13193
Pierre Thierry <pierre@nothos.net>
2010-06-08 15:42:20 +0200

    Corrections in DEFCONSTRAINT

--- constraint.lisp
+++ constraint.lisp
@@ -5,12 +5,14 @@
 ARGS is a list of the form ((name1 type1) (name2 type2) ... (nameN typeN))"
   (with-unique-names (sys-var)
     `(progn
-       (store-predicate-signature ,universe ,name ',(mapcar #'second args))
-       (store-predicate-criteria ,universe ,name
+       (store-predicate-signature ,universe ',name ',(mapcar #'second args))
+       (store-predicate-criterion ,universe ',name
 				 (lambda (entities)
 				   (destructuring-bind ,(mapcar #'first args) entities
 				     (lambda (sys1 sys2)
 				       (macrolet ((system () ,sys-var))
-					 (labels ((score (,sys-var)
+					 (labels ((entity (name)
+						    (get-entity ,sys-var name))
+						  (score (,sys-var)
 						    ,@body))
 					   (funcall ,predicate (score sys1) (score sys2)))))))))))
