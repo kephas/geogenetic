2fcbbba45aa9fd030e63622d5c2da81251abf721
Pierre Thierry <pierre@nothos.net>
2010-06-08 15:14:10 +0200

    Documented DEFCONSTRAINT and changed the use of a user-supplied variable name by a fixed macro

--- constraint.lisp
+++ constraint.lisp
@@ -1,12 +1,16 @@
 (in-package :thierry-technologies.com/2010/06/gene-gcs)
 
-(defmacro defconstraint (universe name args predicate sys-var &body body)
-  `(progn
-     (store-predicate-signature ,universe ,name ',(mapcar #'second args))
-     (store-predicate-criteria ,universe ,name
-			       (lambda (entities)
-				 (destructuring-bind ,(mapcar #'first args) entities
-				   (lambda (sys1 sys2)
-				     (labels ((score (,sys-var)
-						,@body))
-				       (funcall ,predicate (score sys1) (score sys2)))))))))
+(defmacro defconstraint (universe name args predicate &body body)
+  "Defines a named constraint within a geometrical universe.
+ARGS is a list of the form ((name1 type1) (name2 type2) ... (nameN typeN))"
+  (with-unique-names (sys-var)
+    `(progn
+       (store-predicate-signature ,universe ,name ',(mapcar #'second args))
+       (store-predicate-criteria ,universe ,name
+				 (lambda (entities)
+				   (destructuring-bind ,(mapcar #'first args) entities
+				     (lambda (sys1 sys2)
+				       (macrolet ((system () ,sys-var))
+					 (labels ((score (,sys-var)
+						    ,@body))
+					   (funcall ,predicate (score sys1) (score sys2)))))))))))
