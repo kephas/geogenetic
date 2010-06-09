d3a136849b425c3d505b14790f2688c6dec6625a
Pierre Thierry <pierre@nothos.net>
2010-06-09 16:58:08 +0200

    Documented the system definition syntax and MAKE-CRITERIA function

--- sysdef.lisp
+++ sysdef.lisp
@@ -1,5 +1,51 @@
 (in-package :thierry-technologies.com/2010/06/gene-gcs)
 
+#|
+
+System definition
+=================
+
+A system definition file contains a series of forms defining systems
+and valuations. All forms are independent.
+
+
+A system definition form is as follows:
+
+(gcs
+ OPTIONS*)
+
+OPTIONS = CONSTRAINTS-OPT | PARAMETERS-OPT | UNKNOWNS-OPT ;
+
+CONSTRAINTS-OPT = (constraints CONSTRAINT*)
+CONSTRAINT = (PREDICATE ENTITY*)
+
+PARAMETERS-OPT = (parameters ENTITY-SPEC*)
+UNKNOWNS-OPT = (unknowns ENTITY-SPEC*)
+
+ENTITY-SPEC = ENTITY | (TYPE ENTITY*)
+
+PREDICATE, ENTTY and TYPE are symbols. Specifying the type of an entity
+is not needed if it can be infered from its use in a constraint. (e.g. if
+there is a constraint (distcc x y z) and the signature of distcc is
+(distance circle circle), then x will be known as a distance)
+
+
+A valuation definition form is as follows:
+
+(values
+ VALUE* | NAMED-VALUE*)
+
+NAMED-VALUE = (ENTITY VALUE)
+
+VALUE may be any form that can be read by the value function for the type the
+corresponding entity.
+
+If the values are provided without name, they must be in the same order and of
+the same number than in the parameters option of the system definition for which
+they are used.
+
+|#
+
 (defun add-entities-from-specifications (system specifications unknown)
   (named-let loop-specs ((specifications specifications))
     (if specifications
@@ -26,6 +72,10 @@
 	 (get-predicate-signature system (first specification)))))
 
 (defun make-criteria (system specifications)
+  "Returns the list of functions that will evaluate the fitness of
+a solution, with respect to each constraint.
+
+SPECIFICATIONS is the constraints part of the system definition."
   (mapcar (lambda (specification)
 	    (funcall (get-predicate-criterion system (first specification)) (rest specification)))
 	  specifications))
