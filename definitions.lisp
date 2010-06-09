a597f91b1d68ee9c52f2e171ff20fde20c16a8e6
Pierre Thierry <pierre@nothos.net>
2010-06-09 17:09:05 +0200

    Corrected comparison predicate for distpp

--- definitions.lisp
+++ definitions.lisp
@@ -2,5 +2,5 @@
 
 (defvar *euclidian* (make-instance 'geometrical-universe))
 
-(defconstraint *euclidian* distpp ((d distance)(p1 point)(p2 point)) >
+(defconstraint *euclidian* distpp ((d distance)(p1 point)(p2 point)) <
   (error-margin (entity d) (distance-between (entity p1) (entity p2))))
