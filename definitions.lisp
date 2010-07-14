118717b267a4d0cd6d243af277e5290ee8cab5b3
Pierre Thierry <pierre@nothos.net>
2010-07-14 05:27:16 +0200

    Added PARSE-VALUE and methods for angles, points and distances in the euclidian universe

--- definitions.lisp
+++ definitions.lisp
@@ -1,5 +1,10 @@
 (in-package :thierry-technologies.com/2010/06/gene-gcs)
 
+(defgeneric parse-value (universe type value)
+  (:documentation "Returns an entity of type TYPE that,
+in UNIVERSE, is described by VALUE."))
+
+
 (defvar *euclidian* (make-instance 'geometrical-universe))
 
 (defconstraint *euclidian* distpp ((d distance)(p1 point)(p2 point)) <
@@ -10,3 +15,18 @@
 
 (defconstraint *euclidian* angleppp ((a angle) (p1 point) (p2 point) (p3 point)) <
   (error-margin a (vectors-angle (2points->vector p2 p1) (2points->vector p2 p3))))
+
+(defmethod parse-value ((universe (eql *euclidian*)) (type (eql 'angle)) (value number))
+  (make-instance 'geometrical-entity-with-value
+		 :type 'angle
+		 :value value))
+
+(defmethod parse-value ((universe (eql *euclidian*)) (type (eql 'point)) (value list))
+  (make-instance 'geometrical-entity-with-coordinates
+		 :type 'point
+		 :coordinates value))
+
+(defmethod parse-value ((universe (eql *euclidian*)) (type (eql 'distance)) (value number))
+  (make-instance 'geometrical-entity-with-value
+		 :type 'distance
+		 :value value))
