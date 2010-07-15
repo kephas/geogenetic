fb2168c7a179dfc8ff76b0c58b4a6b9a9c7d12b5
Pierre Thierry <pierre@nothos.net>
2010-07-15 22:55:34 +0200

    Added PRINT-OBJECT method for GCS

--- debug.lisp
+++ debug.lisp
@@ -1,5 +1,14 @@
 (in-package :thierry-technologies.com/2010/06/gene-gcs)
 
+(defmethod print-object ((object geometrical-constraints-system) stream)
+  (format stream "#<GEOMETRICAL-CONSTRAINTS-SYSTEM ~a>"
+	  (let ((keys))
+	    (maphash (lambda (key value)
+		       (declare (ignore value))
+		       (push key keys))
+		     (slot-value object 'entities))
+	    (reverse keys))))
+
 (defmethod print-object ((object geometrical-entity) stream)
   (format stream "#<GEOMETRICAL ~a .>" (entity-type object)))
 
