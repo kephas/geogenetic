ed3249d9947a86af658833d14b96bc040da6680f
Pierre Thierry <pierre@nothos.net>
2011-09-03 18:41:41 +0200

    Add dimensions and cloning to geometrical universes

--- universe.lisp
+++ universe.lisp
@@ -2,10 +2,16 @@
 
 (defclass geometrical-universe ()
   ((predicate-criteria :initform (make-hash-table))
-   (predicate-signatures :initform (make-hash-table))))
+   (predicate-signatures :initform (make-hash-table))
+   (dimensions :initform nil)))
 
 (define-hash-table-reader geometrical-universe get-predicate-criterion predicate-criteria)
 (define-hash-table-writer geometrical-universe store-predicate-criterion predicate-criteria)
 
 (define-hash-table-reader geometrical-universe get-predicate-signature predicate-signatures)
 (define-hash-table-writer geometrical-universe store-predicate-signature predicate-signatures)
+
+(defmethod shared-clone :after ((object geometrical-universe) (clone geometrical-universe))
+  (setf (slot-value clone 'predicate-criteria) (slot-value object 'predicate-criteria)
+	(slot-value clone 'predicate-signatures) (slot-value object 'predicate-signatures)
+	(slot-value clone 'dimensions) (slot-value object 'dimensions)))
