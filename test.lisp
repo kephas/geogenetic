78a7f9ee400d4fa2777730f557faba1c1582c652
Pierre Thierry <pierre@nothos.net>
2011-09-04 16:55:32 +0200

    Add function and variables to easily test system definition

---
+++ test.lisp
@@ -0,0 +1,9 @@
+(in-package :thierry-technologies.com/2010/06/gene-gcs)
+
+(defvar %sys)
+(defvar %constraints)
+
+(defun read-test (file)
+  (let ((data (cdar (read-definitions-files file *euclidian*))))
+    (setf %sys (first data)
+	  %constraints (second data))))
