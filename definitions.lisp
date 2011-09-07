493a554b58f82cb86bffc5d3ab75097abe07c1b0
Pierre Thierry <pierre@nothos.net>
2011-09-07 16:38:41 +0200

    Correct DEFCONSTRAINT calling
    Non-existant < variable was passed instead of comparison function #'<

--- definitions.lisp
+++ definitions.lisp
@@ -6,13 +6,13 @@
 (defvar *euclidian-base* (make-instance 'euclidian-universe))
 
 
-(defconstraint *euclidian-base* distpp ((d distance)(p1 point)(p2 point)) <
+(defconstraint *euclidian-base* distpp ((d distance)(p1 point)(p2 point)) #'<
   (error-margin (entity d) (distance-between (entity p1) (entity p2))))
 
-(defconstraint *euclidian-base* anglell ((a angle)(l1 line) (l2 line)) <
+(defconstraint *euclidian-base* anglell ((a angle)(l1 line)(l2 line)) #'<
   (error-margin (entity a) (lines-angle (entity l1) (entity p2))))
 
-(defconstraint *euclidian-base* angleppp ((a angle) (p1 point) (p2 point) (p3 point)) <
+(defconstraint *euclidian-base* angleppp ((a angle) (p1 point) (p2 point) (p3 point)) #'<
   (error-margin a (vectors-angle (2points->vector p2 p1) (2points->vector p2 p3))))
 
 
