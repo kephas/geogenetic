fe88d3da485d50f841079cef05e2f657d25a93f5
Pierre Thierry <pierre@nothos.net>
2011-09-07 03:39:27 +0200

    Add distinction between individuals and their genes in EVOLVE

--- evolve.lisp
+++ evolve.lisp
@@ -1,4 +1,7 @@
 (in-package :thierry-technologies.com/2010/06/gene-gcs)
 
-(defun evolve (population select crossover mutate)
-  (mapcar mutate (mapcan crossover (funcall select population))))
+(defun submapcar (function list)
+  (mapcar (lambda (sublist) (mapcar function sublist)) list))
+
+(defun evolve (population select crossover mutate individual->genes genes->individual)
+  (mapcar genes->individual (mapcar mutate (mapcan crossover (submapcar individual->genes (funcall select population))))))
